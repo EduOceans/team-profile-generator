ba9a6f5ccd0c79dd80606a6a9962a5e2cf6121f4
EduOceans <150608334+EduOceans@users.noreply.github.com>
2024-02-27 15:21:27 +0000

    Adding functionality, effective test, modified README, json packages

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # team-profile-generator
+![EduOceans](https://img.shields.io/badge/Edu-Oceans-blue)
+
+Welcome to the Team Profile Generator project! This command-line application is designed to help you efficiently generate an HTML page that displays information about your team. Whether you're a project manager organizing your team or a developer practicing your coding skills, this project provides a simple and effective way to create a professional-looking team profile.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [License](#license)
+
+## Getting Started
+
+To run the website locally, follow these steps:
+
+1. Clone the repository:
+   ```
+   mkdir module-12
+   cd module-12
+   git clone git@github.com:EduOceans/team-profile-generator.git
+   ```
+
+## Usage
+You can have a look at the website [here](https://eduoceans.github.io/team-profile-generator)
+
+
+## License
+Distributed under the MIT License. See LICENSE.txt for more information.
---
+++ index.js
@@ -0,0 +1,159 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+import render from './page-template.js';
+const team = [];
+
+function createManager() {
+  inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "Enter Manager's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Manager's ID:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter Manager's Email Address:"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter Office number:",
+    },
+  ])
+  .then((answers) => {
+    const { managerName, id, email, officeNumber } = answers;
+    const manager = team.push(new Manager(managerName, id, email, officeNumber));
+    console.log("Manager", manager);
+    menu();
+  })
+}
+function menu() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: "menu",
+      message: "Select Option",
+      choices: [{
+        name:"Add an engineer",
+        value: "engineer",
+      }, 
+                {
+        name: "Add an intern",
+        value: "intern",
+      }, 
+                {
+        name: "Finish building the team",
+        value: "finish",
+      }],
+    }
+  ])
+  .then((answers) => {
+    if(answers.menu === "engineer") {
+      createEngineer();
+    } else if(answers.menu === "intern"){
+      createIntern();
+    } else if(answers.menu === "finish"){
+      console.log('Team:', team);
+      const html = render(team)
+      fs.appendFile('team.html', html, function (err) {
+        if (err) throw err;
+      });
+    }
+  })
+}
+function createEngineer(){
+  inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "Enter Engineer's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Engineers's ID:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter Engineer's Email Address:"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter GitHub Account:",
+    },
+  ])
+  .then((answers) => {
+    const { engineerName, id, email, github } = answers;
+    const engineer = team.push(new Engineer(engineerName, id, email, github));
+    console.log("Engineer", engineer);
+    menu();
+  })
+}
+function createIntern(){
+  inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'internName',
+        message: "Enter Intern's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter Intern's ID:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter Intern's Email Address:"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Enter School:",
+    },
+  ])
+  .then((answers) => {
+    const { internName, id, email, school } = answers;
+    const intern = team.push(new Intern(internName, id, email, school));
+    console.log("Intern", intern);
+    menu();
+  })
+}
+createManager();
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+
+// // If needed 
+// .catch((error) => {
+//   console.log(error);
+//   if (error.isTtyError) {
+//     // Prompt couldn't be rendered in the current environment
+//   } else {
+//     // Something else went wrong
+//   }
+// });
